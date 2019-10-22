49e6d2b8681317fb2944ffe9950a02765a9b9c2e
vikas1015 <31708542+vikas1015@users.noreply.github.com>
2019-10-22 17:52:17 -0400

    Create pythonversionprogram.py

---
+++ pythonversionprogram.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import platform
+
+def main():
+    message()
+    
+def message():
+    print('This is python version {}'.format(platform.python_version()))
+    
+if __name__ == '__main__': main()
